d22bc4b11d1c629d54ab619e7d184506ef5e6979
Timothy Miamba <timal0361@gmail.com>
2023-12-11 16:56:19 +0200

    Add basic structure to page using the template index.html

--- src/console.js
+++ src/console.js
@@ -1,7 +1,6 @@
 export default function viewWeatherData(location) {
   console.log(`Weather Data for ${location.name}`);
   console.table(location);
-  console.log(location.getDate());
   console.log(location.getTime());
   console.log(location.getFormattedDate());
 }
--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import fetchData from "./api";
 import viewWeatherData from "./console";
+import "./styles.css";
 
 fetchData("lilongwe").then((response) => {
   viewWeatherData(response);
